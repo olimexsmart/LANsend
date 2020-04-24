16eacbe433eac33658847e7ce81b5e59ef699c39
Luca Olivieri <luca.olivieri-1@unitn.it>
2020-04-24 12:21:29 +0200

    Reusing socket - previous commit was stupid

--- src/receiver.js
+++ src/receiver.js
@@ -2,6 +2,8 @@
 // This server listens for start file transfer requests
 const httpServer = http.createServer((request, response) => {
 
+    const senderAddress = request.socket.remoteAddress
+
     // Collect data from POST body 
     let body = [];
     request.on('error', err => {
@@ -13,95 +15,78 @@ const httpServer = http.createServer((request, response) => {
     request.on('end', () => {
         // Number of file sent and total size
         const data = JSON.parse(Buffer.concat(body).toString())
-        const nFiles = data.nFiles
+        const fileBaseNames = data.fileBaseNames
         const totSize = data.totSize
+        const nFiles = fileBaseNames.length
 
         // Check if there is enough space to receive the file
         // TODO we need to know where we are saving the file first
 
-
-
         // Create HTTP server that will handle this transfer
         // Count how many file we have received so far
-        let t = 0
-        const transferServer = http.createServer((request, response) => {
-            // Collect fileName from POST body 
-            let body = [];
-            request.on('error', err => {
-                console.error(err);
+
+
+
+        // Create the server that will listen for the incoming file
+        const server = net.createServer()
+
+        let f = 0 // Counter of how many file were received
+        // Write on file received data
+        server.on('connection', socket => {
+            // The connections must be incoming only from the sender
+            // that made the HTTP request
+            if (senderAddress != socket.remoteAddress) {
+                socket.destroy()
+                return
+            }
+
+            // saveFolder is a global variable
+            let filePath = saveFolder + '/' + fileBaseNames[f]
+            f++
+            // saveFolder is a global variable defined in logic.js
+            let fileStream = fs.createWriteStream(filePath);
+            //let progressChecker = null
+
+            fileStream.on('ready', () => {
+                socket.pipe(fileStream);
+
+                // progressChecker = setInterval(fileStreamToCheck => {
+                //     progress = fileStreamToCheck.bytesWritten / size
+                //     // This needs to be updated in GUI
+                //     console.log(progress)
+                // }, 200, fileStream)
             })
-            request.on('data', chunk => {
-                body.push(chunk);
-            })
-            request.on('end', () => {
 
-                let fileName = JSON.parse(Buffer.concat(body).toString()).fileName
-                // saveFolder is a global variable
-                let filePath = saveFolder + '/' + fileName
+            // Close server when file is sent
+            socket.on('end', () => {
+                //clearInterval(progressChecker)
 
-                // Create the server that will listen for the incoming file
-                const server = net.createServer()
-
-                // Write on file received data
-                server.on('connection', socket => {
-                    // saveFolder is a global variable defined in logic.js
-                    let fileStream = fs.createWriteStream(filePath);
-                    let progressChecker = null
-                    fileStream.on('ready', () => {
-                        socket.pipe(fileStream);
-
-                        // progressChecker = setInterval(fileStreamToCheck => {
-                        //     progress = fileStreamToCheck.bytesWritten / size
-                        //     // This needs to be updated in GUI
-                        //     console.log(progress)
-                        // }, 200, fileStream)
+                // If all the file were received, close server
+                if (f >= nFiles) {
+                    server.close(() => {
+                        console.log("Server closed")
                     })
-
-                    // Close server when file is sent
-                    socket.on('end', () => {
-                        clearInterval(progressChecker)
-                        server.close(() => {
-                            t++
-                            if (t == nFiles)
-                                transferServer.close()
-                        });
-                    })
-                })
-
-                server.on('error', error => {
-                    console.log(error)
-                })
-
-                server.listen({
-                    port: 0
-                }, () => {
-                    // When the server is initialized
-                    // send the HTTP response with the port choosen
-                    response.writeHead(200);
-                    response.end(server.address().port.toString());
-
-                    // Set a callback to close server if no file were sent
-                    setTimeout(checkInactiveServer, 3000, server)
-                });
+                }
             })
         })
 
-        // Init this server and tell its port to the sender
-        let tranHandle = null
-        transferServer.listen(() => {
+        server.on('error', error => {
+            console.log(error)
+        })
+
+        server.listen({
+            port: 0
+        }, () => {
+            // When the server is initialized
+            // send the HTTP response with the port choosen
             response.writeHead(200);
-            response.end(transferServer.address().port.toString());
+            response.end(server.address().port.toString());
 
             // Set a callback to close server if no file were sent
-            // TODO 
-            //tranHandle = setInterval(checkInactiveServer, 3000, server)
-        })
-
-        transferServer.on('close', () => {
-            clearInterval(tranHandle)
-        })
-    });
-});
+            setTimeout(checkInactiveServer, 3000, server)
+        });
+    })
+})
 
 // Bind HTTP server to a know port
 httpServer.listen({
--- src/sender.js
+++ src/sender.js
@@ -1,8 +1,12 @@
 function sendFiles(IP, fileNames, totSize) {
 
   // Preparing request notifing receiver that we want 
+  let fileBaseNames = []
+  for (let f = 0; f < fileNames.length; ++f) {
+    fileBaseNames.push(path.basename(fileNames[f]))
+  }
   const data = JSON.stringify({
-    nFiles: fileNames.length,
+    fileBaseNames: fileBaseNames,
     totSize: totSize
   })
 
@@ -23,69 +27,33 @@ function sendFiles(IP, fileNames, totSize) {
     })
     response.on('end', async () => {
       // TODO this string could contain some errors
-      // HTTP port of our private server managing this transfer
-      let transferServerPort = Buffer.concat(body).toString()
+      let port = parseInt(Buffer.concat(body).toString())
 
       // Send file one by one
       for (let f = 0; f < fileNames.length; ++f) {
-        // Tell the receiver the name of the file we are about to send
-        let transferData = JSON.stringify({
-          fileName: path.basename(fileNames[f])
-        })
-
         // Wait until this file is sent
         await new Promise((resolve, reject) => {
-          // Ask for port where to send this file 
-          const transferClient = http.request({
-            host: IP,
-            port: transferServerPort,
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              'Content-Length': transferData.length
-            }
-          }, response => {
-            // Here we are receiving the port of the socket to send data to
-            let body = []
-            response.on('data', chunk => {
-              body.push(chunk);
-            })
-            response.on('end', () => {
-              // Port opened by the receiver listening for our data
-              let port = parseInt(Buffer.concat(body).toString())
-              
-              const socket = net.connect(port, IP)
+          const socket = net.connect(port, IP)
 
-              socket.on('error', error => {
-                console.log(error)
-                reject(error)
-              })
-
-              // Open file to send
-              const fileStream = fs.createReadStream(fileNames[f]);
-
-              // Send file
-              fileStream.on('open', () => {
-                // This just pipes the read stream to the socket, closing it too
-                fileStream.pipe(socket);
-              });
-
-              // Socket closed automagically upon receiving FIN
-              socket.on('close', () => {
-                console.log("Socket closed")
-                resolve()
-              })
-
-            })
-          })
-
-          transferClient.on('error', error => {
+          socket.on('error', error => {
             console.log(error)
+            reject(error)
           })
 
-          // Send the request for this file
-          transferClient.write(transferData)
-          transferClient.end()
+          // Open file to send
+          const fileStream = fs.createReadStream(fileNames[f]);
+
+          // Send file
+          fileStream.on('open', () => {
+            // This just pipes the read stream to the socket, closing it too
+            fileStream.pipe(socket);
+          });
+
+          // Socket closed automagically upon receiving FIN
+          socket.on('close', () => {
+            console.log("Socket closed")
+            resolve()
+          })
         })
       }
     })
